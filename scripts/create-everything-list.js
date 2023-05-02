ec0f19caf185fb40d6875128a820393c0e289201
sefinek24 <sefinek5916@gmail.com>
2023-05-03 01:08:34 +0200

    Update

--- scripts/create-everything-list.js
+++ scripts/create-everything-list.js
@@ -29,16 +29,17 @@ const generatedFolder = path.join(__dirname, '..', 'blocklist', 'generated');
 
 	const domainsCount = domains.size;
 
-	let content = `# ---------------------------------------------------------------------------------------------------------------------------------------------------
+	let content = `# -------------------------------------------------------------------------------------------------------------------------------------------------------
 #
-#       _____   ______   ______   _____   _   _   ______   _  __        ____    _         ____     _____   _  __  _        _____    _____   _______
-#      / ____| |  ____| |  ____| |_   _| | \\ | | |  ____| | |/ /       |  _ \\  | |       / __ \\   / ____| | |/ / | |      |_   _|  / ____| |__   __|
-#     | (___   | |__    | |__      | |   |  \\| | | |__    | ' /        | |_) | | |      | |  | | | |      | ' /  | |        | |   | (___      | |
-#      \\___ \\  |  __|   |  __|     | |   | . \` | |  __|   |  <         |  _ <  | |      | |  | | | |      |  <   | |        | |    \\___ \\     | |
-#      ____) | | |____  | |       _| |_  | |\\  | | |____  | . \\        | |_) | | |____  | |__| | | |____  | . \\  | |____   _| |_   ____) |    | |
-#     |_____/  |______| |_|      |_____| |_| \\_| |______| |_|\\_\\       |____/  |______|  \\____/   \\_____| |_|\\_\\ |______| |_____| |_____/     |_|
+#      _____   ______   ______   _____   _   _   ______   _  __        ____    _         ____     _____   _  __  _        _____    _____   _______
+#     / ____| |  ____| |  ____| |_   _| | \\ | | |  ____| | |/ /       |  _ \\  | |       / __ \\   / ____| | |/ / | |      |_   _|  / ____| |__   __|
+#    | (___   | |__    | |__      | |   |  \\| | | |__    | ' /        | |_) | | |      | |  | | | |      | ' /  | |        | |   | (___      | |
+#     \\___ \\  |  __|   |  __|     | |   | . \` | |  __|   |  <         |  _ <  | |      | |  | | | |      |  <   | |        | |    \\___ \\     | |
+#     ____) | | |____  | |       _| |_  | |\\  | | |____  | . \\        | |_) | | |____  | |__| | | |____  | . \\  | |____   _| |_   ____) |    | |
+#    |_____/  |______| |_|      |_____| |_| \\_| |______| |_|\\_\\       |____/  |______|  \\____/   \\_____| |_|\\_\\ |______| |_____| |_____/     |_|
 #
-#                              The best collection of blocklists for Pi-hole, AdGuard and uBlock Origin - https://sefinek.net
+#                            The best collection of blocklists for Pi-hole, AdGuard and uBlock Origin: https://sefinek.net
+#                                            https://github.com/sefinek24/PiHole-Blocklist-Collection
 #
 # Title       : Big collection of blocklist
 # Description : This list contains a collection of everything from https://github.com/sefinek24/PiHole-BlockList-Test/tree/main/blocklist/default
@@ -49,12 +50,12 @@ const generatedFolder = path.join(__dirname, '..', 'blocklist', 'generated');
 # Version     : ${date.timestamp}-${date.year}${date.month}${date.day}
 # Last update : ${date.hours}:${date.minutes}:${date.seconds}.${date.milliseconds}, ${date.day}.${date.month}.${date.year} [HH:MM:SS.MS, DD.MM.YYYY]
 #
+# > WARNING:
+# By using this file, you agree that the author is not responsible for any damages or losses that may result from its use.
+#
 # > ABOUT:
 # This file is part of the Pi-hole Blocklist Collection maintained by Sefinek.
 # If you encounter any issues or have suggestions, please open an issue on the GitHub repository or contact the author. Feel free to create new Pull Requests.
-#
-# > WARNING:
-# By using this file, you agree that the author is not responsible for any damages or losses that may result from its use.
 #
 # ---------------------------------------------------------------------------------------------------------------------------------------------------\n\n`;
 
