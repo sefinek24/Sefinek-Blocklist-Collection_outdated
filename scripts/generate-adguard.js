0ab0b51fe8de56d3f2fb1a607c7b632f992395c2
sefinek24 <sefinek5916@gmail.com>
2023-05-03 01:37:51 +0200

    Update

--- scripts/generate-adguard.js
+++ scripts/generate-adguard.js
@@ -11,61 +11,61 @@ const convertToAdGuardFormat = async (folderPath = path.join(__dirname, '../bloc
 		await fs.mkdir(adGuardPath, { recursive: true });
 	}
 
+	const files = await fs.readdir(folderPath, { withFileTypes: true });
+	const txtFiles = files.filter(file => file.isFile() && file.name.endsWith('.txt'));
+
+	await Promise.all(txtFiles.map(async file => {
+		const thisFileName = path.join(folderPath, file.name);
+
+		// Cache
+		const cacheFolder = path.join(__dirname, `../cache/${path.basename(path.dirname(thisFileName)) === 'template' ? '' : path.basename(path.dirname(thisFileName))}`);
+		await fs.mkdir(cacheFolder, { recursive: true });
+
+		const cacheFilePath = path.join(cacheFolder, `${file.name.replace('.txt', '')}.hash`);
+
+		let hashFromCacheFile;
+		try {
+			hashFromCacheFile = await fs.readFile(cacheFilePath, 'utf8');
+		} catch (err) {
+			console.warn('❌  Cache file not found:', cacheFilePath);
+		}
+
+		const hashFromTemplateFile = sha256File(thisFileName);
+		console.log(`⏳  File hash: ${hashFromTemplateFile || 'Unknown'}; Cache: ${hashFromCacheFile || 'Unknown'}; File: ${file.name}`);
+
+		if (hashFromTemplateFile === hashFromCacheFile) {
+			console.log(`⏭️ ${thisFileName} - skipped`);
+			return;
+		}
+
+		await fs.writeFile(cacheFilePath, hashFromTemplateFile);
+
+		// Content
+		const fileContent = await fs.readFile(thisFileName, 'utf8');
+		const replacedFile = fileContent
+			.replace(/^# 0\.0\.0\.0 (.*?) (.*)/gmu, '@@||$1^! $2')
+			.replace(/0\.0\.0\.0 (.*?)$/gmu, '||$1^')
+			.replace(/^#/gmu, '!')
+			.replace(/<Release>/gim, 'AdGuard [adguard.com]')
+			.replace(/<Version>/gim, `${date.timestamp}-${date.year}${date.month}${date.day}`)
+			.replace(/<LastUpdate>/gim, `${date.hours}:${date.minutes}:${date.seconds}.${date.milliseconds}, ${date.day}.${date.month}.${date.year} [HH:MM:SS.MS, DD.MM.YYYY]`);
+
+		const generatedFileName = file.name.replace('.txt', '-ags.txt');
+		const subFolderName = path.basename(path.dirname(thisFileName));
+		const categoryPath = subFolderName === 'template' ? adGuardPath : path.join(adGuardPath, subFolderName);
+		const fullNewFile = path.join(categoryPath, generatedFileName);
+
+		try {
+			await fs.access(categoryPath);
+		} catch (err) {
+			await fs.mkdir(categoryPath, { recursive: true });
+		}
+
+		await fs.writeFile(fullNewFile, replacedFile);
+		console.log(`✔️ ${thisFileName}`);
+	}));
+
 	try {
-		const files = await fs.readdir(folderPath, { withFileTypes: true });
-		const textFiles = files.filter(file => file.isFile() && file.name.endsWith('.txt'));
-
-		await Promise.all(textFiles.map(async file => {
-			const thisFileName = path.join(folderPath, file.name);
-
-			// Cache
-			const cacheFolder = path.join(__dirname, `../cache/${path.basename(path.dirname(thisFileName)) === 'template' ? '' : path.basename(path.dirname(thisFileName))}`);
-			await fs.mkdir(cacheFolder, { recursive: true });
-
-			const cacheFilePath = path.join(cacheFolder, `${file.name.replace('.txt', '')}.hash`);
-
-			let hashFromCache;
-			try {
-				hashFromCache = await fs.readFile(cacheFilePath, 'utf8');
-			} catch (err) {
-				console.warn('❌  Cache file not found:', cacheFilePath);
-			}
-
-			const hashDefaultFile = sha256File(thisFileName);
-			console.log(`⏳  File hash: ${hashDefaultFile || 'Unknown'}; Cache: ${hashFromCache || 'Unknown'}; File: ${file.name}`);
-
-			if (hashDefaultFile === hashFromCache) {
-				console.log(`⏭️ ${thisFileName} - skipped`);
-				return;
-			}
-
-			await fs.writeFile(cacheFilePath, hashDefaultFile);
-
-			// Content
-			const fileContents = await fs.readFile(thisFileName, 'utf8');
-			const adGuardFileContents = fileContents
-				.replace(/^# 0\.0\.0\.0 (.*?) (.*)/gmu, '@@||$1^! $2')
-				.replace(/0\.0\.0\.0 (.*?)$/gmu, '||$1^')
-				.replace(/^#/gmu, '!')
-				.replace(/<Release>/gim, 'AdGuard [adguard.com]')
-				.replace(/<Version>/gim, `${date.timestamp}-${date.year}${date.month}${date.day}`)
-				.replace(/<LastUpdate>/gim, `${date.hours}:${date.minutes}:${date.seconds}.${date.milliseconds}, ${date.day}.${date.month}.${date.year} [HH:MM:SS.MS, DD.MM.YYYY]`);
-
-			const adGuardFileName = file.name.replace('.txt', '-ags.txt');
-			const subFolderName = path.basename(path.dirname(thisFileName));
-			const categoryPath = subFolderName === 'template' ? adGuardPath : path.join(adGuardPath, subFolderName);
-			const fullNewFile = path.join(categoryPath, adGuardFileName);
-
-			try {
-				await fs.access(categoryPath);
-			} catch (err) {
-				await fs.mkdir(categoryPath, { recursive: true });
-			}
-
-			await fs.writeFile(fullNewFile, adGuardFileContents);
-			console.log(`✔️ ${thisFileName}`);
-		}));
-
 		const subdirectories = files.filter(file => file.isDirectory());
 		await Promise.all(subdirectories.map(async subdirectory => {
 			await convertToAdGuardFormat(path.join(folderPath, subdirectory.name));
