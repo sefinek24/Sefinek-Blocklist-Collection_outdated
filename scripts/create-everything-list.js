848273304dca8421244c9c8ab9fb72879e0673f3
sefinek24 <sefinek5916@gmail.com>
2023-05-02 21:56:16 +0200

    chuj

--- scripts/create-everything-list.js
+++ scripts/create-everything-list.js
@@ -61,5 +61,5 @@ const generatedFolder = path.join(__dirname, '..', 'blocklist', 'generated');
 	domains.forEach((val) => content += `0.0.0.0 ${val}\n`);
 
 	await fs.writeFile(path.join(generatedFolder, 'everything.txt'), content, 'utf8');
-	console.log(`✔️ Saved everything.txt with ${domainsCount} domains`);
+	console.log(`✔️ Saved everything.txt with ${domainsCount} domains in ${generatedFolder}`);
 })();
