f2d82f61d69d0b7cafab3b31426ccff2ff9cabf5
sefinek24 <sefinek5916@gmail.com>
2023-05-03 03:07:23 +0200

    Update

--- scripts/remove-duplicates.js
+++ scripts/remove-duplicates.js
@@ -1,5 +1,5 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require('node:fs').promises;
+const path = require('node:path');
 
 const processFiles = async dir => {
 	await fs.mkdir(dir, { recursive: true });
