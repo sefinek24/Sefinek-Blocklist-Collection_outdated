db3be274f469efa73f35a4055190cf7cc642d825
sefinek24 <sefinek5916@gmail.com>
2023-05-03 22:20:31 +0200

    Update

--- www/server.js
+++ www/server.js
@@ -11,5 +11,9 @@ console.log(staticPath);
 app.use('/static', express.static(staticPath));
 
 app.listen(process.env.PORT, () => {
-	console.log(`App listening at ${process.env.PROTOCOL}${process.env.DOMAIN}:${process.env.PORT}`);
+	if (process.env.NODE_ENV === 'production') {
+		process.send('ready');
+	} else {
+		console.log(`App listening at ${process.env.PROTOCOL}${process.env.DOMAIN}:${process.env.PORT}`);
+	}
 });
