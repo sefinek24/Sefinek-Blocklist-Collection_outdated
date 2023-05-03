f581fd8fbdd26c84a1e7344ab929dc1e25f44f94
sefinek24 <sefinek5916@gmail.com>
2023-05-03 22:16:54 +0200

    Update

---
+++ www/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+const staticPath = path.join(__dirname, '..', 'blocklist', 'generated');
+console.log(staticPath);
+app.use('/static', express.static(staticPath));
+
+app.listen(process.env.PORT, () => {
+	console.log(`App listening at ${process.env.PROTOCOL}${process.env.DOMAIN}:${process.env.PORT}`);
+});
